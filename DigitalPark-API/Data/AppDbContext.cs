9807fbfdf37887ed7c5fd3912717499e1e30afb5
Martin <martin@ibtech.inf.br>
2021-03-31 13:32:27 -0300

    Started db connection

---
+++ DigitalPark-API/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DigitalPark_API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> User { get; set; }
+
+
+    }
+}
---
+++ DigitalPark-API/Data/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DigitalPark_API.Data
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public String username { get; set; }
+
+        public String password { get; set; }
+
+        public AccessProfile perfilAcesso { get; set; }
+
+
+    }
+}
