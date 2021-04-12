546e9f1b96b5067abcedc8fa2c38fb4c4521ff7f
Martin <martin@ibtech.inf.br>
2021-04-12 13:35:54 -0300

    WIP

---
+++ Data/DigitalParkContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace digitalpark.Data
+{
+    public class DigitalParkContext : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=tcp:digitalpark.database.windows.net,1433;Initial Catalog=digitalpark;Persist Security Info=False;User ID=digital;Password=@park123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+        }
+    }
+}
